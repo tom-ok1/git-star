db52658dc5ad2318c30032f22a2f61ae5f89419e
tom-ok1 <tom.ok1@icloud.com>
2024-10-14 14:11:00 +0900

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
+          fontSans.variable,
         )}
       >
         <Provider>
